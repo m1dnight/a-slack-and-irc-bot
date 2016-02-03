0ed07efb8602412306ec27c7279a7ba89c703adc
Yannick Jadoul <Yannick.Jadoul@vub.ac.be>
2016-02-03 19:20:25 +0100

    Added extra arguments for xkcd command to choose which comic to show

--- src/clojo/modules/plugins/xkcd.clj
+++ src/clojo/modules/plugins/xkcd.clj
@@ -3,7 +3,7 @@
             [clojo.utils           :as    utils]
             [clojure.algo.monads   :refer :all]
             [clojure.data.json     :as    json]
-            [clojure.string          :as    string]
+            [clojure.string        :as    string]
             [clojure.tools.logging :as    log]
             [clj-http.client       :as    client]))
 
@@ -13,13 +13,40 @@
 (def slack-api "https://slack.com/api/")
 (def post-message-method "chat.postMessage")
 
+(defn- parse-args
+  [args]
+  {:requested-num (when args (or (if-let [match (or (re-find #"\b(\d+)\s+(?:back|ago)\b" args) (re-find #"\B-(\d+)\b" args))] (- (read-string (nth match 1))))
+                                 (if-let [match (re-find #"\b(\d+)\b" args)] (read-string (nth match 1)))))})
+
+(defn- get-json-from-url
+  ([url]
+   (get-json-from-url url client/get))
+  ([url method]
+   (domonad maybe-m
+            [raw (try (:body (method url))
+                      (catch Exception e
+                      (log/error "HTTP request" url "failed" (.getMessage e))
+                       nil))
+             json (try (utils/keywordize-keys (json/read-str raw))
+                       (catch Exception e
+                         (log/error "Failed to parse JSON! Given input:" raw)
+                         nil))]
+             json)))
+
 (defn- get-xkcd
-  []
-  (let [url (str xkcd-url xkcd-json)]
-    (try (:body (client/get url))
-         (catch Exception e
-           (log/error "GET" url "failed" (.getMessage e))
-           nil))))
+  ([]
+   (get-json-from-url (str xkcd-url xkcd-json)))
+  ([num]
+   (get-json-from-url (str xkcd-url num "/" xkcd-json))))
+
+(defn- absolute-num
+  [num]
+  (let [max-num (:num (get-xkcd))]
+    (if (> num 0)
+      (if (<= num max-num)
+        num)
+      (if (< (- num) max-num)
+        (+ max-num num)))))
 
 (defn- message-attachments
   [xkcd]
@@ -28,9 +55,9 @@
             img   (:img xkcd)
             alt   (:alt xkcd)
             num   (:num xkcd)]
-           (let [url (str "http://www.xkcd.com/" num)]
+           (let [url (str xkcd-url num "/")]
              [{"fallback" (str title " - " url)
-               "title" title
+               "title" (str title)
                "title_link" url
                "image_url" img
                "color" "#96A8C8"}
@@ -40,19 +67,14 @@
 
 (defn- post-attachments
   [instance channel attachments]
-  (let [url (str slack-api post-message-method)]
-    (try
-      (let [raw-reply (:body (client/post url {:form-params {:token (:token @instance)
-                                                      :channel channel
-                                                      :text ""
-                                                      :as_user true
-                                                      :attachments (json/write-str attachments)}}))
-            reply (utils/keywordize-keys (json/read-str raw-reply))]
-        (when (false? (:ok reply))
-          (log/error "Slack API reply not ok" (:error reply))))
-      (catch Exception e
-        (log/error "POST" url "failed" (.getMessage e))
-        nil))))
+  (let [url (str slack-api post-message-method)
+        reply (get-json-from-url url #(client/post % {:form-params {:token (:token @instance)
+                                                                    :channel channel
+                                                                    :text ""
+                                                                    :as_user true
+                                                                    :attachments (json/write-str attachments)}}))]
+    (when (false? (:ok reply))
+      (log/error "Slack API reply not ok" (:error reply)))))
 
 (m/defmodule                                                                                   
   :xkcd
@@ -61,7 +83,11 @@
     "xkcd"                                                                                    
     (fn [instance args msg]
       (domonad maybe-m
-               [json (get-xkcd)
-                xkcd (utils/keywordize-keys (json/read-str json))
+               [parsed-args (parse-args args)
+                xkcd (if-let [requested-num (:requested-num parsed-args)]
+                       (if-let [abs-num (absolute-num requested-num)]
+                         (get-xkcd abs-num)
+                         (m/reply instance msg "That's not valid xkcd comic, dummy!"))
+                       (get-xkcd))
                 attachments (message-attachments xkcd)]
                (post-attachments instance (:channel msg) attachments)))))
